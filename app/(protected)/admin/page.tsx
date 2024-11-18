9c79557f92352ef5375d66e97b5e5e8cf688f98f
Navid <navid.behroozimajd78@gmail.com>
2024-11-18 16:48:52 +0330

    updated

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,15 +1,8 @@
 import AdminLayout from "@/layouts/AdminLayout";
 import AdminPageTitle from "./_components/AdminPageTitle";
 import { Card, CardContent } from "@/components/ui/card";
-import bcrypt from "bcrypt";
 
 const AdminPage = () => {
-  const masih = bcrypt.hashSync("Masih123!@#", 12);
-  const mahbobe = bcrypt.hashSync("Mahbobe123!@#", 12);
-
-  console.log("masih => ", masih);
-  console.log("mahbobe => ", mahbobe);
-
   return (
     <AdminLayout>
       <AdminPageTitle
