1ff0e08908433184dc9492c707ec9f8f96173040
Navid <navid.behroozimajd78@gmail.com>
2024-11-24 21:22:52 +0330

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,11 @@
 const nextConfig = {
   reactStrictMode: false,
   output: "standalone",
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL || "",
-    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL || "",
-    JWT_SECRET: process.env.JWT_SECRET || "",
-  },
+  // env: {
+  //   DATABASE_URL: process.env.DATABASE_URL || "",
+  //   NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL || "",
+  //   JWT_SECRET: process.env.JWT_SECRET || "",
+  // },
   // images: {
   //   minimumCacheTTL: 900,
   //   remotePatterns: [
